e310067e7495d1d742c476439745b88f5849eedf
Remi Henache <pro.henache@gmail.com>
2024-02-07 18:16:12 +0100

    Ajout exemple DI

--- GestionHotel.Apis/Program.cs
+++ GestionHotel.Apis/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<SampleInjectionInterface, SampleInjectionImplementation>();
 
 var app = builder.Build();
 
